e5ba70b262d9bb53abb788a7062a3748cef09263
Cleidson <cleidsonestevamdasilva@hotmail.com>
2022-01-25 23:09:52 -0300

    Primeira Migration

--- PortalDeVendas/Startup.cs
+++ PortalDeVendas/Startup.cs
@@ -37,7 +37,7 @@ namespace PortalDeVendas
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<PortalDeVendasContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("PortalDeVendasContext")));
+                    options.UseSqlServer(Configuration.GetConnectionString("PortalDeVendasContext"), builder => builder.MigrationsAssembly("PortalDeVendas")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
